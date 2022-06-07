c4364c737f92ab1455adab9f282dfbde0a15a06e
Pankhuri <pankhuritrikha@gmail.com>
2022-06-07 11:45:31 +0530

    Monsters-Rolodex Component State

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,30 @@
+import  {Component}  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+constructor() {
+  super(); //calls the constructor of class component
+
+  this.state = {
+    name: 'Pankhuri'
+  }
+}
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Change Name</button>
       </header>
     </div>
   );
+ }
 }
 
 export default App;

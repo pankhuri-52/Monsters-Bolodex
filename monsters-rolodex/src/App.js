f5ff2baf7a2867bfd31b083877057535fc3c2d80
Pankhuri <pankhuritrikha@gmail.com>
2022-08-25 13:54:06 +0530

    Searching and Filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -38,7 +38,16 @@ render() {
     <input className='search-box' 
             type='search' 
             placeholder='Search monsters' 
-            onChange={(event) => {console.log(event.target.value);}} />
+            onChange={(event) => {console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+                 return monster.name.toLocaleLowerCase().includes(searchString);
+              })
+
+              this.setState(() => {
+                  return {monsters : filteredMonsters};
+              });
+            }} />
       {
         this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>

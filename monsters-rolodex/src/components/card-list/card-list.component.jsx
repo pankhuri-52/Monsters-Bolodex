e6635b6c5b5af04326c236a9ad081bbfbe18c273
Pankhuri <pankhuritrikha@gmail.com>
2022-09-04 17:22:04 +0530

    Fixed the Image Card

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -12,11 +12,11 @@ class CardList extends Component {
                 const {name, email, id} = monster;
                 return (
               <div className="card-container" key={id}>
-                <img alt={`monster ${name}`} 
-                     src={`https://robohash.org/${id}?set=set2`} />
+                    <img alt={`monster ${name}`} 
+                        src={`https://robohash.org/${id}?set=set2`} className="image-styling" />
 
-                <h2>{name}</h2>
-                <p>{email}</p>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
                </div>
                 );
                 })}
